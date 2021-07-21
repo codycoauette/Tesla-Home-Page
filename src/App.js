51a18d9b8d3a402e80a8a32e0b354e6ed87403f7
Cody Coauette <codycoauette@Codys-MacBook-Pro.local>
2021-07-21 09:39:37 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Item from "./components/Item";
+
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="app__itemContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$70,000"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+        />
+        <Item
+          title="Model 3"
+          desc="$75,000"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+        />
+        <Item
+          title="Model X"
+          desc="$80,000"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+        />
+        <Item
+          title="Model Y"
+          desc="$90,000"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+        />
+        <Item
+          title="Solar for New Roofs"
+          desc="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="TRUE"
+        />
+        <Item
+          title="Accessories"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt="SHOP NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+        />
+      </div>
     </div>
   );
 }
